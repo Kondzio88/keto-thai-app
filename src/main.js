ded08fa6c1a838a3044805ceb58168dcfc2af0f2
Kondzio88 <jacoszekjacoszek@o2.pl>
2026-08-05 17:24:17 +0200

    feat: mobile menu and whole nav bar

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
-import {initRouter} from './router.js'
+import { initRouter } from "./router.js";
 
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener("DOMContentLoaded", () => {
+    // INIT RUTER
+    initRouter();
 
-    initRouter()
+    // MOBILE MENU FUNC
+    const menuBtn = document.getElementById("mobile-menu-btn");
+    const mainNav = document.getElementById("main-nav");
 
-})
+    if (menuBtn && mainNav) {
+        menuBtn.addEventListener("click", () => {
+            menuBtn.classList.toggle("is-active");
+            mainNav.classList.toggle("is-active");
+        });
+
+        mainNav.addEventListener("click", (event) => {
+            if (event.target.matches(".header__link")) {
+                menuBtn.classList.remove("is-active");
+                mainNav.classList.remove("is-active");
+            }
+        });
+    }
+});
